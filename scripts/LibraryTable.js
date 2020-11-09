262690c29587974494560cbd15c36b342f273737
John Gibson <jg20019@gmail.com>
2020-11-09 00:49:15 -0500

    Refactor BookCard out of LibraryTable

--- scripts/LibraryTable.js
+++ scripts/LibraryTable.js
@@ -38,38 +38,3 @@ Library.LibraryTable = function (el) {
     } 
 }; 
 
-Library.BookCard = function (el) {
-    let state = {
-        title: 'title', 
-        author: 'author', 
-        pages: '1', 
-        read: true
-    } 
-
-    el.innerHTML = `
-        <div class="book-card"> 
-          <span class="title"></span> 
-          <span class="author"></span> 
-          <span class="pages"></span> 
-          <span class="read"></span> 
-        </div> 
-    `; 
-
-    let titleEl = el.querySelector('.title'); 
-    let authorEl = el.querySelector('.author'); 
-    let pagesEl = el.querySelector('.pages'); 
-    let readEl = el.querySelector('.read'); 
-
-    el.BookCard = { update } 
-
-    return el; 
-
-    function update(next){
-        Object.assign(state, next); 
-
-        titleEl.innerText = state.title; 
-        authorEl.innerText = state.author; 
-        pagesEl.innerText = state.pages; 
-        readEl.innerText = state.read ? 'You have read this book' : 'You have not read this book'; 
-    }
-} 
