1a6c792ce13f67fc9d09c6b8fcebd9b47037c7d6
John Gibson <jg20019@gmail.com>
2020-11-09 00:42:07 -0500

    LibraryTable renders a Book

--- scripts/LibraryTable.js
+++ scripts/LibraryTable.js
@@ -2,13 +2,20 @@ window.Library = window.Library || {};
 
 Library.LibraryTable = function (el) {
     let state = {
-        books: []
+        books: [
+            { title: 'A Tale of Two Cities.', 
+              author: 'Charles Dickens', 
+              pages: 295, 
+              read: true
+            }, 
+        ]
     };  
 
     el.innerHTML = `
-        <div> This is where the library will go. </div>
+        <div class="books"></div>
     `; 
-    
+   
+
     update(); 
 
     /* Some message it might want to hear. 
@@ -21,6 +28,47 @@ Library.LibraryTable = function (el) {
      */ 
 
     function update(){
+        let booksEl = el.querySelector('.books'); 
+        booksEl.innerHTML = ''; 
+        state.books.forEach(book => {
+            let bookEl = document.createElement('div'); 
+            Library.BookCard(bookEl); 
+            bookEl.BookCard.update(book); 
+            booksEl.appendChild(bookEl); 
+        }); 
     } 
 }; 
 
+Library.BookCard = function (el) {
+    let state = {
+        title: 'title', 
+        author: 'author', 
+        pages: '1', 
+        read: true
+    } 
+
+    el.innerHTML = `
+        <div class="book-card"> 
+          <span class="title"></span> 
+          <span class="author"></span> 
+          <span class="pages"></span> 
+          <span class="read"></span> 
+        </div> 
+    `; 
+
+    let titleEl = el.querySelector('.title'); 
+    let authorEl = el.querySelector('.author'); 
+    let pagesEl = el.querySelector('.pages'); 
+    let readEl = el.querySelector('.read'); 
+
+    el.BookCard = { update } 
+
+    function update(next){
+        Object.assign(state, next); 
+
+        titleEl.innerText = state.title; 
+        authorEl.innerText = state.author; 
+        pagesEl.innerText = state.pages; 
+        readEl.innerText = state.read ? 'You have read this book' : 'You have not read this book'; 
+    }
+} 
